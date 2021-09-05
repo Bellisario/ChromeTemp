f743c22c118f54af01d3c74342b3e21b6ae9ded0
Bellisario <72039923+Bellisario@users.noreply.github.com>
2021-09-05 19:11:22 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,26 @@
 @echo off
 
 set build="%cd%\temps\build"
+if [%1] == [] goto :versionDefault
+set "version=%1"
+echo Using %version% as version for build
+echo.
+goto :endVersion
+:versionDefault
+echo Not specified any version, using default
+echo.
+set version=default
+:endVersion
 echo|set /p="Building... "
 if not exist %build% mkdir %build%
 xcopy /S .\app\*.* %build%\app\ /Y>nul || goto :error
 copy .\app\first-run\RUNME.bat %build%\RUNME.bat>nul || goto :error
 mkdir %build%\temps || goto :error
 copy .\temps\README.txt %build%\temps\README.txt>nul || goto :error
+
+REM Write version file
+echo %version% > %build%\app\extensions\update-checker\version.txt
+
 echo OK.
 echo|set /p="Making zip... "
 if exist .\temps\chrome-temp.zip del .\temps\chrome-temp.zip
